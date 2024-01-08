a0980514de058e5bf3ad3da016347b1be6e4a2f3
Caua <cauahenrique5@outlook.com>
2024-01-08 10:22:16 -0300

    add css clean code details

--- src/pages/Details.jsx
+++
@@ -1,6 +0,0 @@
-// no return eu coloco o conteudo que será exibido
- export default function Details(){
-  return (
-     <h1>hello word</h1>
-  )
-}
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,7 @@
+import "./styles.css"
+
+export function Details(){
+    return (
+        <h1>hello word</h1>
+    )
+}
