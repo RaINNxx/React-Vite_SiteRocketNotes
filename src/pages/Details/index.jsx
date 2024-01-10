88c1549d797368a147a824eeed498f1bcf635502
Caua <cauahenrique5@outlook.com>
2024-01-10 10:04:47 -0300

    vizualizando o botao na tela

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
 import { Container } from './styles'
 
+import { Button } from '../../components/button'
+
 export function Details(){
     
     return (
         <Container>
          <h1>hello word</h1>
+
+         <Button/>
         </Container>
     )
 }
