09532726afec09415209882105a069c878738a79
Caua <cauahenrique5@outlook.com>
2024-01-11 10:44:40 -0300

    create component Section

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,22 @@
-import { Container } from './styles'
+import { Container, Links } from './styles'
 
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
+import { Section } from '../../components/Section'
 
 export function Details() {
   return (
     <Container>
       <Header />
+      <Section title="Links uteis">
+         <Links>
+          <li>
+          <li> <a href="#">Link 1</a> </li>
+
+          </li>
+         
+         </Links>
+      </Section>
       <Button title="Voltar" />
     </Container>
   )
