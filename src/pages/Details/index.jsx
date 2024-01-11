18239b7c73e5f721033eb1d79fc5b582b1c2f983
Caua <cauahenrique5@outlook.com>
2024-01-11 15:13:21 -0300

    create component Tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,20 +3,29 @@ import { Container, Links } from './styles'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 
 export function Details() {
   return (
     <Container>
       <Header />
-      <Section title="Links uteis">
-         <Links>
-          <li>
-          <li> <a href="#">Link 1</a> </li>
 
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">https://rocketseat.com.br</a>
           </li>
-         
-         </Links>
+          <li>
+            <a href="#">https://rocketseat.com.br</a>
+          </li>
+        </Links>
       </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="node" />
+      </Section>
+
       <Button title="Voltar" />
     </Container>
   )
