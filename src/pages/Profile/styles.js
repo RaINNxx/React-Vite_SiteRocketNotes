7249e3a5161012b7c56a0433f34676a01f9a128c
Caua <cauahenrique5@outlook.com>
2024-01-18 08:50:37 -0300

    Adicionando IMG a page profile

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -25,3 +25,45 @@ export const Form = styled.form`
  margin:30px auto 0;
 
 `
+export const Avatar = styled.div`
+ position: relative;
+ margin: -124px auto 32px;
+
+ width: 186px;
+ height: 186px;
+
+ > img {
+    border-radius: 50%;
+    width: 186px;
+    height: 186px;
+ }
+
+ > label {
+    width: 48px;
+    height: 48px;
+
+    background-color: ${({ theme }) => theme.COLORS.ORANGE};
+    border-radius: 50% ;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: absolute;
+    bottom: 7px;
+    right: 7px;
+
+    cursor: pointer;
+
+    input{
+        display: none;
+    }
+
+    svg{
+        width: 20px;
+        height: 20px;
+        color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+
+    }
+ }
+`
