c7207b50b85ad4e6e89bbea889f99d3952bce6fc
Caua <cauahenrique5@outlook.com>
2024-01-18 08:33:22 -0300

    Created Profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,48 @@
-import { Container } from "./styles";
+import { FiArrowLeft,FiUser,FiMail,FiLock  } from 'react-icons/fi'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { Container,Form } from './styles'
 
 export function Profile() {
     return (
         <Container>
+           <header>
+            <a href="">
+            <FiArrowLeft/>
+            </a>
+           </header>
 
+           <Form>
+               <Input 
+                  placeholder="nome"
+                  type="text"
+                  icon={FiUser}
+               />
+
+                <Input 
+                  placeholder="E-mail"
+                  type="text"
+                  icon={FiMail}
+               />
+
+                <Input 
+                  placeholder="Senha Atual"
+                  type="password"
+                  icon={FiLock}
+               />
+
+                <Input 
+                  placeholder="Nova Senha"
+                  type="password"
+                  icon={FiLock}
+               />
+
+               <Button
+                title="Salvar"
+               />
+
+           </Form>
         </Container>
     )
 }
