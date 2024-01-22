4ed1706f5f2e5fa55f5d1b20ce6744513335d2e4
Caua <cauahenrique5@outlook.com>
2024-01-22 10:20:06 -0300

    create components noteitem

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
+import { Textarea } from '../../components/Textarea'
 
 import { Container, Form } from './styles'
 
@@ -12,10 +13,11 @@ export function New(){
                 <Form >
                     <header>
                         <h1>Criar nota</h1>
-                        <a href="/">voltar</a>
+                        <a href='/'>voltar</a>
                     </header>
 
-                    <Input placeholder="Título" />
+                    <Input placeholder='Título' />
+                    <Textarea placeholder='Observações'/>
                 </Form>
             </main>
         </Container>
